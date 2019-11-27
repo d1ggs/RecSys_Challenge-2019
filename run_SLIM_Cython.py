db87722904b54f2444568a114ab531d5bd2bd26f
Diego Piccinotti <elimdor@live.it>
2019-11-27 16:49:40 +0100

    Modified early stopping to return parameters

--- run_SLIM_Cython.py
+++ run_SLIM_Cython.py
@@ -5,65 +5,12 @@ from utils.run import RunRecommender
 
 import matplotlib.pyplot as plt
 
+from threading import Thread
+
+class SLIMRunner(Thread):
+    pass
+
 if __name__ == '__main__':
-    pset_collaborative_filter = [
-        {"top_k": 500, "shrink": 0},
-        {"top_k": 550, "shrink": 0},
-        {"top_k": 600, "shrink": 0},
-        {"top_k": 650, "shrink": 0},
-        {"top_k": 700, "shrink": 0},
-        {"top_k": 750, "shrink": 0},
-        {"top_k": 800, "shrink": 0},
-        {"top_k": 850, "shrink": 0},
-        {"top_k": 900, "shrink": 0},
-        {"top_k": 950, "shrink": 0},
-        {"top_k": 500, "shrink": 2},
-        {"top_k": 550, "shrink": 2},
-        {"top_k": 600, "shrink": 2},
-        {"top_k": 650, "shrink": 2},
-        {"top_k": 700, "shrink": 2},
-        {"top_k": 750, "shrink": 2},
-        {"top_k": 800, "shrink": 2},
-        {"top_k": 850, "shrink": 2},
-        {"top_k": 900, "shrink": 2},
-        {"top_k": 900, "shrink": 2},
-    ]
-
-    pset1 = [{"lr": 0.1, "epochs": 10, "top_k": 5},
-             {"lr": 0.1, "epochs": 20, "top_k": 5},
-             {"lr": 0.1, "epochs": 30, "top_k": 5},
-             {"lr": 0.1, "epochs": 40, "top_k": 5},
-             {"lr": 0.1, "epochs": 50, "top_k": 5},
-             {"lr": 0.1, "epochs": 60, "top_k": 5},
-             {"lr": 0.1, "epochs": 70, "top_k": 5},
-             {"lr": 0.1, "epochs": 80, "top_k": 5},
-             {"lr": 0.1, "epochs": 90, "top_k": 5},
-             {"lr": 0.1, "epochs": 100, "top_k": 5}]
-
-    pset2 = [{"lr": 0.005, "epochs": 10, "top_k": 5},
-             {"lr": 0.005, "epochs": 20, "top_k": 5},
-             {"lr": 0.005, "epochs": 30, "top_k": 5},
-             {"lr": 0.005, "epochs": 40, "top_k": 5},
-             {"lr": 0.005, "epochs": 50, "top_k": 5},
-             {"lr": 0.005, "epochs": 60, "top_k": 5},
-             {"lr": 0.005, "epochs": 70, "top_k": 5},
-             {"lr": 0.005, "epochs": 80, "top_k": 5},
-             {"lr": 0.005, "epochs": 90, "top_k": 5},
-             {"lr": 0.005, "epochs": 100, "top_k": 5}]
-
-    pset3 = [{"lr": 0.001, "epochs": 50, "top_k": 5},
-             {"lr": 0.001, "epochs": 60, "top_k": 5},
-             {"lr": 0.001, "epochs": 70, "top_k": 5},
-             {"lr": 0.001, "epochs": 80, "top_k": 5},
-             {"lr": 0.001, "epochs": 90, "top_k": 5},
-             {"lr": 0.001, "epochs": 100, "top_k": 5},
-             {"lr": 0.001, "epochs": 120, "top_k": 5},
-             {"lr": 0.001, "epochs": 140, "top_k": 5},
-             {"lr": 0.001, "epochs": 160, "top_k": 5},
-             {"lr": 0.001, "epochs": 180, "top_k": 5},
-             {"lr": 0.001, "epochs": 200, "top_k": 5},
-             ]
-
 
     pset5 = [{"lr": 0.001, "epochs": 30, "top_k": 5}]
 
@@ -77,10 +24,43 @@ if __name__ == '__main__':
     pset2 = {"lr": 0.001, "epochs": 1000, "top_k": 5}
     pset = {"lr": 0.001, "epochs": 1000, "top_k": 100}
 
-    recommender = SLIM_BPR_Cython(URM_train, recompile_cython=False, verbose=False)
+    map10 = []
 
-    recommender.fit(epochs=1000, learning_rate=pset["lr"], topK=pset["top_k"], random_seed=1234,
-                    **{"validation_every_n": 10, "stop_on_validation": True,
-                       "validation_metric" : "MAP", "lower_validations_allowed" : 2,
-                       "evaluator_object" : evaluator})
+    params = [{"lr": 0.001, "epochs": 1000, "top_k": 1},
+              {"lr": 0.001, "epochs": 1000, "top_k": 2},
+              {"lr": 0.001, "epochs": 1000, "top_k": 3},
+              {"lr": 0.001, "epochs": 1000, "top_k": 4},
+              {"lr": 0.001, "epochs": 1000, "top_k": 5},
+              {"lr": 0.001, "epochs": 1000, "top_k": 6},
+              {"lr": 0.001, "epochs": 1000, "top_k": 7},
+              {"lr": 0.001, "epochs": 1000, "top_k": 8},
+              {"lr": 0.001, "epochs": 1000, "top_k": 9},
+              {"lr": 0.001, "epochs": 1000, "top_k": 10}]
+
+    for p in params:
+
+        recommender = SLIM_BPR_Cython(URM_train, recompile_cython=False, verbose=False)
+
+        best_MAP, best_epoch = recommender.fit(epochs=1000, learning_rate=p["lr"], topK=p["top_k"], random_seed=1234,
+                        **{"validation_every_n": 10, "stop_on_validation": True,
+                           "validation_metric" : "MAP", "lower_validations_allowed" : 2,
+                           "evaluator_object" : evaluator})
+        print("Parameters:", pset)
+        print("Best epoch:", best_epoch)
+        print("MAP score:", best_MAP)
+        map10.append((best_MAP, best_epoch))
+
+    max_map = 0
+    best_parameters = None
+    best_epochs = 0
+
+    for i in range(len(map10)):
+        if map10[i][0] > max_map:
+            max_map = map10[i][0]
+            best_epochs = map10[1]
+            best_parameters = params[i]
+
+    print("Parameters:", best_parameters)
+    print("Best epoch:", best_epochs)
+    print("MAP score:", max_map)
     # RunRecommender.write_submission(recommender)
