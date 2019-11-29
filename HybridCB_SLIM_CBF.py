a06b0d70ebbbb9fa5dcfa53fd50d63ebaaacc972
Diego Piccinotti <elimdor@live.it>
2019-11-29 11:45:01 +0100

    Fix merge

--- HybridCB_SLIM_CBF.py
+++ HybridCB_SLIM_CBF.py
@@ -1,5 +1,5 @@
 from ItemBasedCBF import ItemBasedCBF
-from CollaborativeFilter import CollaborativeFilter
+from UserCollaborativeFilter import UserCollaborativeFilter
 from SLIM_BPR.Cython.SLIM_BPR_Cython import SLIM_BPR_Cython
 from TopPopularRecommender import TopPopRecommender
 import numpy as np
@@ -35,7 +35,7 @@ class HybridCBCBFSLIMRecommender():
                                 weight_asset=item_cbf_parameters["weight_asset"],
                                 weight_price=item_cbf_parameters["weight_price"],
                                 weight_sub_class=item_cbf_parameters["weight_sub_class"])
-        self.cb = CollaborativeFilter(topK=cb_parameters["topK"], shrink=cb_parameters["shrink"])
+        self.cb = UserCollaborativeFilter(topK=cb_parameters["topK"], shrink=cb_parameters["shrink"])
         self.toppop = TopPopRecommender()
         self.cold_users = Helper().get_cold_user_ids()
 
--- ItemCollaborativeFilter.py
+++ ItemCollaborativeFilter.py
@@ -1,10 +1,12 @@
 from Legacy.Base.Similarity.Compute_Similarity_Python import Compute_Similarity_Python
 import numpy as np
 from utils.helper import Helper
-# from evaluation.Evaluator import Evaluator
-class CollaborativeFilter(object):
+from utils.run import RunRecommender
 
-    def __init__(self, topK=500, shrink=2, normalize=True, similarity = "cosine"):
+# from evaluation.Evaluator import Evaluator
+class ItemCollaborativeFilter(object):
+
+    def __init__(self, topK=500, shrink=2, normalize=True, similarity="cosine"):
         self.URM_train = None
         self.W_sparse = None
         self.topK = topK
@@ -13,7 +15,7 @@ class CollaborativeFilter(object):
         self.similarity = similarity
 
     def compute_similarity_matrix(self, URM_train, shrink, topK, normalize, similarity):
-        similarity_object = Compute_Similarity_Python(URM_train.T, shrink=shrink,
+        similarity_object = Compute_Similarity_Python(URM_train, shrink=shrink,
                                                       topK=topK, normalize=normalize,
                                                       similarity=similarity)
 
@@ -21,15 +23,16 @@ class CollaborativeFilter(object):
 
     def fit(self, URM_train):
         self.URM_train = URM_train
-        self.W_sparse = self.compute_similarity_matrix(self.URM_train, self.shrink, self.topK, self.normalize, self.similarity)
+        self.W_sparse = self.compute_similarity_matrix(self.URM_train, self.shrink, self.topK, self.normalize,
+                                                       self.similarity)
+        self.URM_W_dot = self.URM_train.dot(self.W_sparse)
 
     def compute_scores(self, user_id):
-        return self.W_sparse[user_id, :].dot(self.URM_train).toarray().ravel()
+        return np.squeeze(np.asarray(self.URM_W_dot[user_id].todense()))
 
     def recommend(self, user_id, at=10, exclude_seen=False):
-
         # compute the scores using the dot product
-        
+
         scores = self.compute_scores(user_id)
 
         if exclude_seen:
@@ -37,28 +40,28 @@ class CollaborativeFilter(object):
 
         # rank items
         ranking = scores.argsort()[::-1]
-            
-        return ranking[:10]
-    
-    def filter_seen(self, user_id, scores):
 
+        return ranking[:10]
+
+    def filter_seen(self, user_id, scores):
         start_pos = self.URM_train.indptr[user_id]
-        end_pos = self.URM_train.indptr[user_id+1]
+        end_pos = self.URM_train.indptr[user_id + 1]
 
         user_profile = self.URM_train.indices[start_pos:end_pos]
-        
+
         scores[user_profile] = -np.inf
 
         return scores
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     # evaluator = Evaluator()
     # evaluator.split_data_randomly()
 
     helper = Helper()
-    cb = CollaborativeFilter(topK=210, shrink=0)
+    cb_parameters = {"topK": 2,
+                     "shrink": 20}
+    cb = ItemCollaborativeFilter(topK=cb_parameters["topK"], shrink=cb_parameters["shrink"])
 
-    # map10 = RunRecommender.run(cb) # THIS GENERATES A CIRCULAR IMPORT
+    map10 = RunRecommender.perform_evaluation(cb) # THIS GENERATES A CIRCULAR IMPORT
     # print('{0:.128f}'.format(map10))
-    print(map10)
