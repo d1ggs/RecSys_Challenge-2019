ce8b002cffa4de8b12882a51a144e588bd556979
Matteo Carretta <carretta.mtt@gmail.com>
2020-01-12 16:39:26 +0100

    Hyperopt SLIM BPR

--- hyperopt_SLIM_BPR.py
+++ hyperopt_SLIM_BPR.py
@@ -1,5 +1,5 @@
 import hyperopt as hp
-from hyperopt import Trials, fmin, STATUS_OK
+from hyperopt import Trials, fmin, space_eval, STATUS_OK
 from SLIM_BPR.Cython.SLIM_BPR_Cython import SLIM_BPR_Cython
 from utils.run import RunRecommender
 import numpy as np
@@ -8,12 +8,15 @@ from utils.helper import Helper
 
 helper = Helper()
 
+N_KFOLD = 10
+MAX_EVALS = 100
 
 ### Step 1 : defining the objective function
 def objective(params):
-    print("Current parameters:")
+    params["topK"] = int(params["topK"])
+    print("############ Current parameters ############")
     print(params)
-    loss = - RunRecommender.evaluate_on_validation_set(SLIM_BPR_Cython, params)
+    loss = - RunRecommender.evaluate_on_validation_set(SLIM_BPR_Cython, params, Kfold=N_KFOLD, parallelize_evaluation=True, user_group="warm")
     return loss
 
 slim_cbr_space = {
@@ -26,7 +29,7 @@ slim_cbr_space = {
     "lambda_i": hp.hp.uniform('lambda_i', 0.00, 0.1),
     "lambda_j": hp.hp.uniform('lambda_j', 0.000, 0.1),
     "learning_rate": hp.hp.uniform('learning_rate', 1e-5, 1e-1),
-    "topK": hp.hp.choice('topK', np.arange(0, 1000, 50)),
+    "topK": hp.hp.quniform('topK', 0, 1000, 50),
     "sgd_mode": hp.hp.choice('sgd_mode', ['adagrad']),
     "gamma": hp.hp.uniform('gamma', 0.5, 1.0),
     "beta_1": hp.hp.uniform('beta_1', 0.5, 1.0),
@@ -37,7 +40,6 @@ slim_cbr_space = {
 if __name__ == '__main__':
     ### step 3 : storing the results of every iteration
     bayes_trials = Trials()
-    MAX_EVALS = 50
 
 
 
@@ -48,4 +50,11 @@ if __name__ == '__main__':
 
     ### best will the return the the best hyperparameter set
 
+    best = space_eval(slim_cbr_space, best)
+
+    print("############### Best parameters ###############")
+
+
     print(best)
+    RunRecommender.evaluate_on_test_set(SLIM_BPR_Cython, best, Kfold=N_KFOLD, parallelize_evaluation=True, user_group="warm")
+
